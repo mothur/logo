5366e5da53e7328fe05fe79abd124d1fb3d524d2
Kelly Sovacool <kellysovacool@gmail.com>
2022-03-17 14:44:48 -0400

    Remove raw file create by hexSticker::sticker()

--- hex-sticker/hex-sticker.R
+++ hex-sticker/hex-sticker.R
@@ -6,5 +6,5 @@ img <- ggdraw() +
 sticker(img, package = '', 
         s_x = 1,  s_y = 1, s_width = 2, s_height = 2,
         h_color = '#000000', h_fill = '#002cf4',
-        filename = here('hex-sticker/mothur-logo-hex.png'))
-# TODO: manually cut off the edges outside the hexagon
+        filename = here('hex-sticker/mothur-logo-hex-raw.png'))
+# manually cut off the edges outside the hexagon
