372a90b14a523d90f5410f6159dd14331327b813
Kelly Sovacool <kellysovacool@gmail.com>
2022-03-18 16:03:52 -0400

    Create version w/ original logo enclosed entirely

--- hex-sticker/hex-sticker.R
+++ hex-sticker/hex-sticker.R
@@ -6,5 +6,11 @@ img <- ggdraw() +
 sticker(img, package = '', 
         s_x = 1,  s_y = 1, s_width = 2, s_height = 2,
         h_color = '#000000', h_fill = '#002cf4',
-        filename = here('hex-sticker/mothur-logo-hex-raw.png'))
-# manually cut off the edges outside the hexagon
+        filename = here('hex-sticker/mothur-logo-hex-overhang.png'))
+# have to manually cut off the edges outside the hexagon
+
+# alternatively, reduce the size of the internal image
+sticker(img, package = '', 
+        s_x = 1,  s_y = 1, s_width = 1.84, s_height = 1.84,
+        h_color = '#000000', h_fill = '#002cf4',
+        filename = here('hex-sticker/mothur-logo-hex-enclosed.png'))
